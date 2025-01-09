bbb48f5286dceaefe4495fd5c8d94f8b230c6fd5
alyrajwani <aly_rajwani@brown.edu>
2025-01-09 11:38:57 -0800

    added linear to gamma factoring to color

--- src/color.rs
+++ src/color.rs
@@ -3,16 +3,21 @@ use std::fs::File;
 use std::io::{self, Write};
 
 pub fn write_color(mut file: &File, pixel_color: Point3D) -> io::Result<()> {
-    let r: f64 = pixel_color.x();
-    let g: f64 = pixel_color.y();
-    let b: f64 = pixel_color.z();
+    let r_lin: f64 = pixel_color.x();
+    let g_lin: f64 = pixel_color.y();
+    let b_lin: f64 = pixel_color.z();
+    
+    // Apply a linear to gamma transform for gamma 2
+    let r_gam = linear_to_gamma(r_lin);
+    let g_gam = linear_to_gamma(g_lin);
+    let b_gam = linear_to_gamma(b_lin);
 
     // Translate the [0,1] component values to the byte range [0,255].
     let min: f64 = 0.000;
     let max: f64 = 0.999;
-    let rbyte: usize = (256.0 * clamp(r, min, max)) as usize;
-    let gbyte: usize = (256.0 * clamp(g, min, max)) as usize;
-    let bbyte: usize = (256.0 * clamp(b, min, max)) as usize;
+    let rbyte: usize = (256.0 * clamp(r_gam, min, max)) as usize;
+    let gbyte: usize = (256.0 * clamp(g_gam, min, max)) as usize;
+    let bbyte: usize = (256.0 * clamp(b_gam, min, max)) as usize;
 
     // Write out the pixel color components.
     writeln!(file, "{} {} {}", rbyte, gbyte, bbyte)?;
@@ -26,5 +31,13 @@ pub fn clamp(x: f64, min: f64, max: f64) -> f64 {
         max
     } else {
         x
+    }
+}
+
+pub fn linear_to_gamma(linear_component: f64) -> f64 {
+    if linear_component > 0.0 {
+        f64::sqrt(linear_component)
+    } else {
+        0.0
     }
 }
