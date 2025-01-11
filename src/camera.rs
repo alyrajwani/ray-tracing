aaa1902f446b5f1c66aa489cfbf165ca50aeee10
alyrajwani <aly_rajwani@brown.edu>
2025-01-11 06:10:22 -0800

    refactored camera definition to be less verbose

--- src/camera.rs
+++ src/camera.rs
@@ -6,47 +6,66 @@ use std::fs::File;
 use std::io::{self, Write};
 use crate::random::*;
 use crate::material::*;
-use rayon::prelude::*;
-use rand::prelude::*;
 
 pub const PI: f64 = 3.14159265358979323846264338327950288_f64;
 
 pub struct Camera {
+    stats: CameraStats,
+    view: CameraView,
+    focus: CameraFocus,
+}
+
+#[derive(Clone, Copy)]
+pub struct CameraStats {
+    aspect_ratio: f64,
+    image_height: f64,
+    image_width: f64,
+    pixel_samples_scale: f64,
+    max_depth: usize,
+}
+
+#[derive(Clone, Copy)]
+pub struct CameraView {
     center: Point3D,
     pixel00_loc: Point3D,
     pixel_delta_u: Point3D,
     pixel_delta_v: Point3D,
-    image_height: f64,
-    image_width: f64,
-    aspect_ratio: f64,
-    pixel_samples_scale: f64,
-    max_depth: usize,
-    vfov: f64, // Vertical view angle (field of view)
-    lookfrom: Point3D, // Point camera is looking from
-    lookat: Point3D, // Point camera is looking at
-    vup: Point3D, // Camera-relative "up" direction
-    defocus_angle: f64, // Variation angle of rays through each pixel
+    vfov: f64,
+    lookfrom: Point3D,
+    lookat: Point3D,
+    vup: Point3D,
     focus_dist: f64, // Distance from camera lookfrom point to plane of perfect focus
+}
+
+#[derive(Clone, Copy)]
+pub struct CameraFocus {
+    defocus_angle: f64, // Variation angle of rays through each pixel
     defocus_disk_u: Point3D, // Defocus disk horizontal radius 
     defocus_disk_v: Point3D, // Defocus disk vertical radius
 }
 
-impl Camera {
-    pub fn new(aspect_ratio: f64, image_width: f64, samples_per_pixel: f64, max_depth: usize, 
-        vfov: f64, lookfrom: Point3D, lookat: Point3D, vup: Point3D, defocus_angle: f64, focus_dist: f64) -> Camera {
+impl CameraStats {
+    pub fn new(aspect_ratio: f64, image_width: f64, samples_per_pixel: f64, max_depth: usize) -> CameraStats {
         let image_height = if image_width / aspect_ratio < 1.0 {
             1.0 
         } else {
             image_width / aspect_ratio
         };
+        let pixel_samples_scale = 1.0 / samples_per_pixel;
+    
+        CameraStats { aspect_ratio, image_height, image_width, pixel_samples_scale, max_depth } 
+    }
+}
 
+impl CameraView {
+    pub fn new(stats: CameraStats, vfov: f64, lookfrom: Point3D, lookat: Point3D, vup: Point3D, focus_dist: f64) -> CameraView {
         let center = lookfrom;
 
         // Determine viewport dimensions.
         let theta = Camera::degrees_to_radians(vfov);
         let h = (theta / 2.0).tan();
         let viewport_height: f64 = 2.0 * h * focus_dist;
-        let viewport_width: f64 = viewport_height * (image_width / image_height);
+        let viewport_width: f64 = viewport_height * (stats.image_width / stats.image_height);
 
         // Calculate the u,v,w unit basis vectors for the camera coordinate frame.
         let w = (lookfrom - lookat).unit_vector();
@@ -58,40 +77,37 @@ impl Camera {
         let viewport_v = -v * viewport_height;   // Vector down viewport vertical edge 
 
         // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-        let pixel_delta_u = viewport_u / image_width;
-        let pixel_delta_v = viewport_v / image_height;
+        let pixel_delta_u = viewport_u / stats.image_width;
+        let pixel_delta_v = viewport_v / stats.image_height;
 
         // Calculate the location of the upper left pixel.
         let viewport_upper_left = center - (w * focus_dist) - viewport_u / 2.0 - viewport_v / 2.0;
         let pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v) * 0.5;
+        
+        // Calculate the location of the upper left pixel.
+        CameraView { center, pixel00_loc, pixel_delta_u, pixel_delta_v, vfov, lookfrom, lookat, vup, focus_dist }
+    }
+}
+
+impl CameraFocus {
+    pub fn new(view: CameraView, defocus_angle: f64) -> CameraFocus {
+        // Calculate the u,v,w unit basis vectors for the camera coordinate frame.
+        let w = (view.lookfrom - view.lookat).unit_vector();
+        let u = view.vup.cross(&w).unit_vector();
+        let v = w.cross(&u);
 
         // Calculate the camera defocus disk basis vectors.
-        let defocus_radius = focus_dist * Camera::degrees_to_radians(defocus_angle / 2.0).tan();
+        let defocus_radius = view.focus_dist * Camera::degrees_to_radians(defocus_angle / 2.0).tan();
         let defocus_disk_u = u * defocus_radius;
         let defocus_disk_v = v * defocus_radius;
 
-        // Determine how many rays hit each point.
-        let pixel_samples_scale = 1.0 / samples_per_pixel;
+        CameraFocus { defocus_angle, defocus_disk_u, defocus_disk_v }
+    }
+}
 
-        Camera {
-            center,
-            pixel00_loc,
-            pixel_delta_u,
-            pixel_delta_v,
-            image_height,
-            image_width,
-            aspect_ratio,
-            pixel_samples_scale,
-            max_depth,
-            vfov,
-            lookfrom,
-            lookat,
-            vup,
-            defocus_angle, 
-            focus_dist,
-            defocus_disk_u, 
-            defocus_disk_v,
-        }
+impl Camera {
+    pub fn new(stats: CameraStats, view: CameraView, focus: CameraFocus) -> Camera {
+        Camera { stats, view, focus }
     }
 
     pub fn render(&self, world: &dyn Hittable) -> io::Result<()> {
@@ -100,18 +116,18 @@ impl Camera {
         let path: String = format!("{}/{}.ppm", output_dir, file_name);
         let mut file = File::create(path)?;
 
-        writeln!(file, "P3\n{} {}\n255", self.image_width, self.image_height)?;
+        writeln!(file, "P3\n{} {}\n255", self.stats.image_width, self.stats.image_height)?;
 
-        let samples_per_pixel = (1.0 / self.pixel_samples_scale) as usize;
-        for j in 0..self.image_height as usize {
-            for i in 0..self.image_width as usize {
+        let samples_per_pixel = (1.0 / self.stats.pixel_samples_scale) as usize;
+        for j in 0..self.stats.image_height as usize {
+            for i in 0..self.stats.image_width as usize {
                 let mut pixel_color = Point3D::new(0.0, 0.0, 0.0);
                 for _ in 0..samples_per_pixel {
                     let r = self.get_ray(i as f64, j as f64);
-                    pixel_color = pixel_color + self.ray_color(&r, self.max_depth, world);
+                    pixel_color = pixel_color + self.ray_color(&r, self.stats.max_depth, world);
                 }
 
-                write_color(&file, pixel_color * self.pixel_samples_scale)?;
+                write_color(&file, pixel_color * self.stats.pixel_samples_scale)?;
             }
         }
         Ok(())
@@ -138,17 +154,17 @@ impl Camera {
         // Construct a camera ray originating from the defocus disk and directed at a randomly
         // sampled point around the pixel location i, j.
         let offset = self.sample_square();
-        let pixel_sample = self.pixel00_loc  
-            + (self.pixel_delta_u * (i + offset.x()))
-            + (self.pixel_delta_v * (j + offset.y()));
-        let ray_origin = if self.defocus_angle <= 0.0 { self.center } else { self.defocus_disk_sample() }; 
+        let pixel_sample = self.view.pixel00_loc  
+            + (self.view.pixel_delta_u * (i + offset.x()))
+            + (self.view.pixel_delta_v * (j + offset.y()));
+        let ray_origin = if self.focus.defocus_angle <= 0.0 { self.view.center } else { self.defocus_disk_sample() }; 
         let ray_direction = pixel_sample - ray_origin;
         Ray::new(ray_origin, ray_direction)
     }
 
     fn defocus_disk_sample(&self) -> Point3D {
         let p = Point3D::random_in_unit_disk();
-        self.center + (self.defocus_disk_u * p.x()) + (self.defocus_disk_v * p.y())
+        self.view.center + (self.focus.defocus_disk_u * p.x()) + (self.focus.defocus_disk_v * p.y())
     }
 
 
--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,32 @@ use crate::material::*;
 use crate::random::*;
 pub const PI: f64 = 3.14159265358979323846264338327950288_f64;
 
-fn main() -> io::Result<()> {
+fn three_balls() -> HittableList {
+    let mut world = HittableList::new(Vec::new());
+
+    let material_ground = Material::Lambertian(Lambertian{ albedo: Point3D::new(0.8, 0.8, 0.0) });
+    let material_center = Material::Lambertian(Lambertian{ albedo: Point3D::new(0.1, 0.2, 0.5) });
+    let material_left = Material::Dielectric(Dielectric{ refraction_index: 1.50 });
+    let material_bubble = Material::Dielectric(Dielectric{ refraction_index: 1.00 / 1.50 }); 
+    let material_right = Material::Metal(Metal{ albedo: Point3D::new(0.8, 0.6, 0.2), fuzz: 1.0 });
+
+    world.list.push(Rc::new(Sphere::new(Point3D::new(0.0, -100.5, -1.0), 100.0, material_ground)));
+    world.list.push(Rc::new(Sphere::new(Point3D::new(0.0, 0.0, -1.2), 0.5, material_center)));
+    world.list.push(Rc::new(Sphere::new(Point3D::new(-1.0, 0.0, -1.0), 0.5, material_left)));
+    world.list.push(Rc::new(Sphere::new(Point3D::new(-1.0, 0.0, -1.0), 0.4, material_bubble)));
+    world.list.push(Rc::new(Sphere::new(Point3D::new(1.0, 0.0, -1.0), 0.5, material_right)));
+
+    world
+}
+
+fn random_scene() -> HittableList {
     let mut world = HittableList::new(Vec::new());
     
-    for a in -11..=11 {
-        for b in -11..=11 {
+    let ground_material = Material::Lambertian(Lambertian { albedo: Point3D::new(0.5, 0.5, 0.5) } );
+    world.list.push(Rc::new(Sphere::new(Point3D::new(0.0, -1000.0, 0.0), 1000.0, ground_material)));
+
+    for a in -11..11 {
+        for b in -11..11 {
             let choose_mat = random_f64();
             let center = Point3D::new(0.9 * random_f64() + (a as f64), 0.2, 0.9 * random_f64() + (b as f64));
 
@@ -42,7 +63,7 @@ fn main() -> io::Result<()> {
             } else {
                 None
             };
-            
+
             if let Some(material) = sphere_material { 
                 world.list.push(Rc::new(Sphere::new(center, 0.2, material)));
             }
@@ -58,18 +79,58 @@ fn main() -> io::Result<()> {
     let material_three = Material::Metal(Metal { albedo: Point3D::new(0.7, 0.6, 0.5), fuzz: 0.0 } );
     world.list.push(Rc::new(Sphere::new(Point3D::new(4.0, 1.0, 0.0), 1.0, material_three)));
 
+    world
+}
+
+fn camera_three_ball() -> Camera {
     let aspect_ratio: f64 = 16.0 / 9.0;
-    let image_width: f64 = 1200.0;
-    let samples_per_pixel: f64 = 500.0;
+    let image_width: f64 = 400.0;
+    let samples_per_pixel: f64 = 100.0;
     let max_depth: usize = 50;
+   
+    let stats = CameraStats::new(aspect_ratio, image_width, samples_per_pixel, max_depth);
+
+    let vfov: f64 = 20.0;
+    let lookfrom = Point3D::new(-2.0, 2.0, 1.0);
+    let lookat = Point3D::new(0.0, 0.0, -1.0);
+    let vup = Point3D::new(0.0, 1.0, 0.0);
+    let focus_dist = 10.0;
+    
+    let view = CameraView::new(stats, vfov, lookfrom, lookat, vup, focus_dist);
+
+    let defocus_angle = 0.0;
+
+    let focus = CameraFocus::new(view, defocus_angle);
+    
+    Camera::new(stats, view, focus)
+}
+
+fn camera_random() -> Camera {
+    let aspect_ratio: f64 = 16.0 / 9.0;
+    let image_width: f64 = 800.0;
+    let samples_per_pixel: f64 = 10.0;
+    let max_depth: usize = 20;
+
+    let stats = CameraStats::new(aspect_ratio, image_width, samples_per_pixel, max_depth);
 
     let vfov: f64 = 20.0;
     let lookfrom = Point3D::new(13.0, 2.0, 3.0);
     let lookat = Point3D::new(0.0, 0.0, 0.0);
     let vup = Point3D::new(0.0, 1.0, 0.0);
-    let defocus_angle = 0.6;
     let focus_dist = 10.0;
-    let camera = Camera::new(aspect_ratio, image_width, samples_per_pixel, max_depth, vfov, lookfrom, lookat, vup, defocus_angle, focus_dist);
+
+    let view = CameraView::new(stats, vfov, lookfrom, lookat, vup, focus_dist);
+    
+    let defocus_angle = 0.6;
+    
+    let focus = CameraFocus::new(view, defocus_angle);
+    
+    Camera::new(stats, view, focus)
+}
+
+fn main() -> io::Result<()> {
+    let world = random_scene(); 
+    let camera = camera_random();
 
     let _ = camera.render(&world);
 
